6c02a891de7c4a7e894721e9dcb5d0ed636073df
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2023-10-07 12:36:57 +0530

    Add files via upload

---
+++ maincode/App.js
@@ -0,0 +1,13 @@
+import MyComponent from './MyComponent.js';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="App">
+      <MyComponent />
+    </div>
+    </div>
+  );
+}
+
+export default App;
